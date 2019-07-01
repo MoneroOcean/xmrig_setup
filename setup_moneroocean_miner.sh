c4460f480dbad979e73c6ec9938ba2a2d4d0e2c7
MoneroOcean <support@moneroocean.stream>
2019-07-01 10:15:29 -0700

    Some reliability improvements

--- setup_moneroocean_miner.sh
+++ setup_moneroocean_miner.sh
@@ -254,10 +254,11 @@ sed -i 's/"url": *"[^"]*",/"url": "gulf.moneroocean.stream:'$PORT'",/' $HOME/mon
 sed -i 's/"user": *"[^"]*",/"user": "'$WALLET'",/' $HOME/moneroocean/config.json
 sed -i 's/"pass": *"[^"]*",/"pass": "'$PASS'",/' $HOME/moneroocean/config.json
 sed -i 's/"max-cpu-usage": *[^,]*,/"max-cpu-usage": 100,/' $HOME/moneroocean/config.json
+sed -i 's/"retries": *[^,]*,/"retries": 0,/' $HOME/moneroocean/config.json
+sed -i 's#"log-file": *null,#"log-file": "'$HOME/moneroocean/xmrig.log'",#' $HOME/moneroocean/config.json
 
 cp $HOME/moneroocean/config.json $HOME/moneroocean/config_background.json
 sed -i 's/"background": *false,/"background": true,/' $HOME/moneroocean/config_background.json
-sed -i 's#"log-file": *null,#"log-file": "'$HOME/moneroocean/xmrig.log'",#' $HOME/moneroocean/config_background.json
 
 # preparing script
 
@@ -302,7 +303,7 @@ else
 Description=Monero miner service
 
 [Service]
-ExecStart=$HOME/moneroocean/xmrig
+ExecStart=$HOME/moneroocean/xmrig --config=$HOME/moneroocean/config.json
 Nice=10
 
 [Install]
