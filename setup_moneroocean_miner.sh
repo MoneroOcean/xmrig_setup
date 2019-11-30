8c471395f0a5153d8f2f628ddb2b9cff70280526
MoneroOcean <support@moneroocean.stream>
2019-11-29 20:45:13 -0800

    Fixed stuff for new lscpu

--- setup_moneroocean_miner.sh
+++ setup_moneroocean_miner.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=2.5
+VERSION=2.6
 
 # printing greetings
 
@@ -67,33 +67,45 @@ if [ -z $CPU_SOCKETS ]; then
   export CPU_SOCKETS=1
 fi
 CPU_CORES_PER_SOCKET=`echo "$LSCPU" | grep "^Core(s) per socket:" | cut -d':' -f2 | sed "s/^[ \t]*//"`
-if [ -z $CPU_CORES_PER_SOCKET ]; then
+if [ -z "$CPU_CORES_PER_SOCKET" ]; then
   echo "WARNING: Can't get CPU cores per socket from lscpu output"
   export CPU_CORES_PER_SOCKET=1
 fi
 CPU_THREADS=`echo "$LSCPU" | grep "^CPU(s):" | cut -d':' -f2 | sed "s/^[ \t]*//"`
-if [ -z $CPU_THREADS ]; then
+if [ -z "$CPU_THREADS" ]; then
   echo "WARNING: Can't get CPU cores from lscpu output"
   export CPU_THREADS=1
 fi
 CPU_MHZ=`echo "$LSCPU" | grep "^CPU MHz:" | cut -d':' -f2 | sed "s/^[ \t]*//"`
 CPU_MHZ=${CPU_MHZ%.*}
-if [ -z $CPU_MHZ ]; then
+if [ -z "$CPU_MHZ" ]; then
   echo "WARNING: Can't get CPU MHz from lscpu output"
   export CPU_MHZ=1000
 fi
-CPU_L1_CACHE=`echo "$LSCPU" | grep "^L1d" | cut -d':' -f2 | sed "s/^[ \t]*//" | sed "s/K\$//"`
-if [ -z $CPU_L1_CACHE ]; then
+CPU_L1_CACHE=`echo "$LSCPU" | grep "^L1d" | cut -d':' -f2 | sed "s/^[ \t]*//" | sed "s/ \?K\(iB\)\?\$//"`
+if echo "$CPU_L1_CACHE" | grep MiB >/dev/null; then
+  CPU_L1_CACHE=`echo "$CPU_L1_CACHE" | sed "s/ MiB\$//"`
+  CPU_L1_CACHE=$(( $CPU_L1_CACHE * 1024))
+fi
+if [ -z "$CPU_L1_CACHE" ]; then
   echo "WARNING: Can't get L1 CPU cache from lscpu output"
   export CPU_L1_CACHE=16
 fi
-CPU_L2_CACHE=`echo "$LSCPU" | grep "^L2" | cut -d':' -f2 | sed "s/^[ \t]*//" | sed "s/K\$//"`
-if [ -z $CPU_L2_CACHE ]; then
+CPU_L2_CACHE=`echo "$LSCPU" | grep "^L2" | cut -d':' -f2 | sed "s/^[ \t]*//" | sed "s/ \?K\(iB\)\?\$//"`
+if echo "$CPU_L2_CACHE" | grep MiB >/dev/null; then
+  CPU_L2_CACHE=`echo "$CPU_L2_CACHE" | sed "s/ MiB\$//"`
+  CPU_L2_CACHE=$(( $CPU_L2_CACHE * 1024))
+fi
+if [ -z "$CPU_L2_CACHE" ]; then
   echo "WARNING: Can't get L2 CPU cache from lscpu output"
   export CPU_L2_CACHE=256
 fi
-CPU_L3_CACHE=`echo "$LSCPU" | grep "^L3" | cut -d':' -f2 | sed "s/^[ \t]*//" | sed "s/K\$//"`
-if [ -z $CPU_L3_CACHE ]; then
+CPU_L3_CACHE=`echo "$LSCPU" | grep "^L3" | cut -d':' -f2 | sed "s/^[ \t]*//" | sed "s/ \?K\(iB\)\?\$//"`
+if echo "$CPU_L3_CACHE" | grep MiB >/dev/null; then
+  CPU_L3_CACHE=`echo "$CPU_L3_CACHE" | sed "s/ MiB\$//"`
+  CPU_L3_CACHE=$(( $CPU_L3_CACHE * 1024))
+fi
+if [ -z "$CPU_L3_CACHE" ]; then
   echo "WARNING: Can't get L3 CPU cache from lscpu output"
   export CPU_L3_CACHE=2048
 fi
